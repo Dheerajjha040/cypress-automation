64d5ac518071723000345eb4fea74614b32f1b26
dheeraj <dheerajjha040@gmail.com>
2025-08-20 15:51:51 +0530

    parallel

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -7,7 +7,7 @@ describe('parabanktestuite', () => {
 
   it('Verify the login page opened', () => {
 
-    cy.get('.logo').should('not.be.visible')
+    cy.get('.logo').should('not.visible')
     cy.get('p').contains('Experience the difference').should('be.visible')
   
   })
